88c3a04d4c9118e2d7599c44dd4528160e2237ca
matixk <matixk73@gmail.com>
2018-12-19 12:17:03 +0100

    Entity abstract class for feature file browser classes added

---
+++ SanityArchiver/SanityArchiver.Application/Models/Entity.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+
+namespace SanityArchiver.Application.Models
+{
+    public abstract class Entity : INotifyPropertyChanged
+    {
+        private string name;
+        private string path;
+        
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("name");
+            }
+        }
+        public string Path
+        {
+            get => path;
+            set
+            {
+                path = value;
+                OnPropertyChanged("path");
+            }
+        }
+
+        public Entity(string path)
+        {
+            Path = path;
+        }
+        
+        /// <summary> Returns the occupied space on disk in bytes </summary>
+        /// <returns> long </returns>
+        public abstract long GetSize();
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
