a887b803ca625dcb9466db6e5d7a992c65453b0c
matixk <matixk73@gmail.com>
2018-12-19 15:39:48 +0100

    Drives initialization added

--- SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
@@ -1,13 +1,38 @@
+using System;
 using System.Collections.ObjectModel;
-using SanityArchiver.Application.Models.Directories;
-using SanityArchiver.Application.Models.Files;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Directory = SanityArchiver.Application.Models.Directories.Directory;
+using File = SanityArchiver.Application.Models.Files.File;
 
 namespace SanityArchiver.DesktopUI.ViewModels
 {
     public class MainWindowVM
     {
-        public ObservableCollection<Directory> Directories { get; set; }
-        public ObservableCollection<File> Files { get; set; }
+        public ObservableCollection<Directory> Directories { get; }
+        public ObservableCollection<File> Files { get; }
         
+        public MainWindowVM()
+        {
+            Directories = new ObservableCollection<Directory>();
+            try
+            {
+                DriveInfo.GetDrives().ToList().ForEach(disk =>
+                {
+                    if (!disk.IsReady) return;
+                    var directory = new Directory(disk.RootDirectory.FullName);
+                    directory.LoadSubDirectories();
+                    Directories.Add(directory);
+                });
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "Drivers loading failed",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            Files = new ObservableCollection<File>();
+
+        }
     }
 }
