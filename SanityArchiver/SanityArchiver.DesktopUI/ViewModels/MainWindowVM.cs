48a2a655ae1cf1098383601c41d3c72c97dfbf65
matixk <matixk73@gmail.com>
2018-12-19 17:15:18 +0100

    Command for expanding directory TreeView added.

--- SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using SanityArchiver.Application.Models;
 using Directory = SanityArchiver.Application.Models.Directories.Directory;
 using File = SanityArchiver.Application.Models.Files.File;
 
@@ -12,9 +16,14 @@ namespace SanityArchiver.DesktopUI.ViewModels
     {
         public ObservableCollection<Directory> Directories { get; }
         public ObservableCollection<File> Files { get; }
-        
+        public ICommand ExpandDirectory => new RelayCommand(CanExpand, LoadDirectories);
+
+        /// <summary>
+        /// Creates a new ViewModel used in MainWindow and loads drivers for TreeView.
+        /// </summary>
         public MainWindowVM()
         {
+            Files = new ObservableCollection<File>();
             Directories = new ObservableCollection<Directory>();
             try
             {
@@ -31,8 +40,21 @@ namespace SanityArchiver.DesktopUI.ViewModels
                 MessageBox.Show($"{e.Message}", "Drivers loading failed",
                     MessageBoxButton.OK, MessageBoxImage.Warning);
             }
-            Files = new ObservableCollection<File>();
-
+        }
+        
+        /// <summary> Update TreeView directories. </summary>
+        /// <param name="sender">TreeViewItem which calling this method.</param>
+        private void LoadDirectories(object sender)
+        {
+            Debug.WriteLine(sender);
+            var treeViewItem = (TreeViewItem)sender;
+            ((Directory)treeViewItem.DataContext).LoadSubDirectories();
+        }
+        
+        private bool CanExpand(object arg)
+        {
+            Debug.WriteLine(arg);
+            return true;
         }
     }
 }
