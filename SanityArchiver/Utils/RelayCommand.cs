412ef6790e37e4fb037258249b5425533465cf4d
matixk <matixk73@gmail.com>
2018-12-21 09:18:00 +0100

    Added RelayCommand for future command binding

---
+++ SanityArchiver/Utils/RelayCommand.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Windows.Input;
+
+namespace Utils
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        
+        public void Execute(object parameter) { execute(parameter); }
+    }
+}
