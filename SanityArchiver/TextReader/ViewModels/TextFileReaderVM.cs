b42cdccbfe8655aec215cd16080135b0a8c4dd04
matixk <matixk73@gmail.com>
2018-12-21 09:40:11 +0100

    ViewModel update for window bindings

--- SanityArchiver/TextReader/ViewModels/TextFileReaderVM.cs
+++ SanityArchiver/TextReader/ViewModels/TextFileReaderVM.cs
@@ -1,13 +1,38 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TextReader.Models;
 
 namespace TextReader.ViewModels
 {
-    public class TextFileReaderVM : INotifyPropertyChanged
+    public sealed class TextFileReaderVM : INotifyPropertyChanged
     {
+        public string FileName
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public ICommand Open => throw new System.NotImplementedException();
+
+        public ICommand Save => throw new System.NotImplementedException();
+
+        public ICommand SaveAs => throw new System.NotImplementedException();
+
+        public TextFileReader TextFileReader
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public TextFileReaderVM() => TextFileReader = new TextFileReader();
+        public TextFileReaderVM(string path)
+        {
+            TextFileReader = new TextFileReader(path);
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
