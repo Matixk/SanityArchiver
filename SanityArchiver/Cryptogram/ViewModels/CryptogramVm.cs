194519a94e4785a77c15f73e423183478f5fe489
matixk <matixk73@gmail.com>
2018-12-21 11:45:04 +0100

    Switch to one page app idea

--- SanityArchiver/Cryptogram/ViewModels/CryptogramVm.cs
+++ SanityArchiver/Cryptogram/ViewModels/CryptogramVm.cs
@@ -1,22 +1,91 @@
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 using System.Windows.Input;
-using Cryptogram.Views;
 using Utils;
 
 namespace Cryptogram.ViewModels
 {
-    public class CryptogramVm
+    public class CryptogramVm : INotifyPropertyChanged
     {
-        public ICommand Encrypt => new RelayCommand(OpenEncryptDialog);
-        public ICommand Decrypt => new RelayCommand(OpenDecryptDialog);
+        private string path = "";
+        private string password = "";
 
-        private void OpenEncryptDialog(object obj)
+        public string Path
         {
-            new Encrypt().Show();
+            get => path;
+            set
+            {
+                path = File.Exists(value) ? value : "";
+                OnPropertyChanged("path");
+            }
+        }
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value ?? "";
+                OnPropertyChanged("password");
+            }
         }
 
-        private void OpenDecryptDialog(object obj)
+        public ICommand FileBrowser => new RelayCommand(SelectFile);
+        public ICommand Action => new RelayCommand(ActionWithFile);
+
+        public CryptogramVm() {}
+
+        public CryptogramVm(string path = null) => Path = path;
+
+        private void SelectFile(object obj)
         {
-            new Decrypt().Show();
+            var fileBrowser = new OpenFileDialog
+            {
+                Filter = @"ENC files (*.ENC)|*.ENC|txt files (*.txt)|*.txt|All files (*.*)|*.*", 
+                FilterIndex = 3, RestoreDirectory = true
+            };
+
+            if (fileBrowser.ShowDialog() == DialogResult.OK)
+            {
+                Path = fileBrowser.FileName;
+            }
+        }
+
+        protected void ActionWithFile(object obj)
+        {
+            if (path == null || !File.Exists(path)) return;
+            var extension = new FileInfo(path).Extension;
+
+            switch (extension)
+            {
+                case ".txt":
+                    EncryptFile();
+                    break;
+                case ".ENC":
+                    DecryptFile();
+                    break;
+                default:
+                    MessageBox.Show($"{extension} must be .txt or .ENC", "Action",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void EncryptFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DecryptFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
