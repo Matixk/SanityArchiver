3db663fc7ccaf0dda49d2d67765de0ed5066f8c4
matixk <matixk73@gmail.com>
2018-12-21 09:25:14 +0100

    TextReader project init

---
+++ SanityArchiver/TextReader/ViewModels/TextFileReaderVM.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TextReader.ViewModels
+{
+    public class TextFileReaderVM : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
