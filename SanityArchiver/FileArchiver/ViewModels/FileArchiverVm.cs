eadeae0d02c4021ce0d727fd0bf6df26de45ecf7
matixk <matixk73@gmail.com>
2018-12-21 14:23:47 +0100

    Added new window for compressor

--- SanityArchiver/FileArchiver/ViewModels/FileArchiverVm.cs
+++ SanityArchiver/FileArchiver/ViewModels/FileArchiverVm.cs
@@ -1,7 +1,16 @@
+using System.Windows.Input;
+using FileArchiver.Views;
+using Utils;
+
 namespace FileArchiver.ViewModels
 {
     public class FileArchiverVm
     {
+        public ICommand Compress => new RelayCommand(OpenCompressDialog);
 
+        private void OpenCompressDialog(object obj)
+        {
+            new Compressor().Show();
+        }
     }
 }
