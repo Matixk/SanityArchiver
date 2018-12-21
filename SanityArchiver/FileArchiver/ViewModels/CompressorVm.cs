5fa479f6111c7229d81dbd17c73cc8d470a09507
matixk <matixk73@gmail.com>
2018-12-21 14:34:52 +0100

    Compressor window added

--- SanityArchiver/FileArchiver/ViewModels/CompressorVm.cs
+++ SanityArchiver/FileArchiver/ViewModels/CompressorVm.cs
@@ -1,7 +1,30 @@
+using FileArchiver.Models.Compressor;
+
 namespace FileArchiver.ViewModels
 {
-    public class CompressorVm
+    public class CompressorVm : DefaultVm
     {
+        private CompressionSpeed speed;
+        
+        public CompressionSpeed Speed
+        {
+            get => speed;
+            set
+            {
+                speed = value;
+                OnPropertyChanged("speed");
+            }
+        }
+        
+        public CompressorVm() {}
+
+        public CompressorVm(string path) : base(path) {}
+        
+        protected override void ActionWithFolder(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
         
     }
 }
