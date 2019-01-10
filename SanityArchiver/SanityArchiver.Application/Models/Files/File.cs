8db9636f097b7dcb38232ef46c4e13cd6986f76c
matixk <matixk73@gmail.com>
2019-01-10 11:13:55 +0100

    Update File.cs and MainWindow.xaml

--- SanityArchiver/SanityArchiver.Application/Models/Files/File.cs
+++ SanityArchiver/SanityArchiver.Application/Models/Files/File.cs
@@ -4,6 +4,22 @@ namespace SanityArchiver.Application.Models.Files
 {
     public class File : Entity
     {
+        public string Icon
+        {
+            get
+            {
+                switch (extension)
+                {
+                    case ".txt":
+                        return $"{IconsFolder}txt.png";
+                    case ".ENC":
+                        return $"{IconsFolder}enc.png";
+                    case ".zip":
+                        return $"{IconsFolder}zip-file.png";
+                }
+                return $"{IconsFolder}file.png";
+            }
+        }
         private string extension;
 
         public string Extension
