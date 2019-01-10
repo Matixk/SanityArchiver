da0610a31aa58b230de9d2e0b6da7adbb8d8117f
matixk <matixk73@gmail.com>
2019-01-10 15:48:10 +0100

    Exe file start added

--- SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainWindowVM.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -224,8 +225,16 @@ namespace SanityArchiver.DesktopUI.ViewModels
             var filePath = file.Path;
             var fileExtension = file.Extension;
 
-            if (".txt".Equals(fileExtension))
-                new MainWindow(filePath).Show();
+            switch (fileExtension)
+            {
+                case ".txt":
+                case ".ENC":
+                    new MainWindow(filePath).Show();
+                    break;
+                case ".exe":
+                    Process.Start(filePath);
+                    break;
+            }
         }
 
         /// <summary> Open Cryptogram dialog for selected file. </summary>
